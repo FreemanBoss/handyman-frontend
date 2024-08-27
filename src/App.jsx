4c9a8c76b68980dbc6ee20877dd0cb0ecf520668
kiuvert39 <forrexplug@gmail.com>
2024-08-27 17:46:05 +0100

    fix: resolve linting issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './Pages/Home.jsx';
 import LoginPage from './Pages/auth/login.page.jsx';
